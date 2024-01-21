85232e334392da29eb8406949cfd84daf2c69bbe
jesusrobot0 <jesusrobot9000@icloud.com>
2024-01-20 22:58:02 -0600

    Add TitlePage component

---
+++ src/components/title-page/TitlePage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface Props {
+  title: string;
+  description?: string;
+}
+
+export function TitlePage({ title, description }: Props) {
+  return (
+    <div>
+      <h1 className="mb-2 text-3xl font-bold text-[#222]">{title}</h1>
+      <p>{description}</p>
+    </div>
+  );
+}

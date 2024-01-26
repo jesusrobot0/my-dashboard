a0aaaccc5bbf3fdc68371b45eeef9f019312d790
jesusrobot0 <jesusrobot9000@icloud.com>
2024-01-26 16:26:56 -0600

    Add the not-found page to handle not-found errors at a general level in the project

---
+++ src/app/not-found.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import Link from "next/link";
+import { Sidebar } from "@/components";
+import { title } from "@/fonts";
+
+export default function NotFound() {
+  return (
+    <div className="h-screen p-8 flex justify-between gap-4">
+      <Sidebar />
+      <div className="w-3/4 px-4 py-8 ">
+        <div className="w-[960px] mt-16 flex flex-col justify-center items-center">
+          <Image
+            src="/img/bob-404.jpeg"
+            alt="Image of spongebob computer on fire"
+            width={957 / 3}
+            height={720 / 2}
+            className=" mb-8 text-red-400"
+          />
+          <h2
+            className={`mb-8 text-9xl text-center ${title.className} text-red-400`}
+          >
+            404
+          </h2>
+          <p className="mb-8 text-2xl">Page Not Found</p>
+          <Link
+            href="/dashboard/home"
+            className="py-4 px-12 rounded-md text-white bg-[#5181da] focus:bg-[#4168b0]"
+          >
+            Go Home
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}

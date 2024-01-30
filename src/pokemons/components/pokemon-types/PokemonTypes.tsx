3a84c812451e96e5e4f1ee6f5aae1c974c062e19
jesusrobot0 <jesusrobot9000@icloud.com>
2024-01-30 13:45:26 -0600

    Fix color types

--- src/pokemons/components/pokemon-types/PokemonTypes.tsx
+++ src/pokemons/components/pokemon-types/PokemonTypes.tsx
@@ -1,10 +1,30 @@
-import { COLOR_TYPES } from "@/constants";
-import { Type } from "@/pokemons/interfaces/pokemon-type";
+import { ColorTypes, Type } from "@/pokemons/interfaces/pokemon-type";
 
 interface Props {
   types: Type[];
   name: string;
 }
+
+const COLOR_TYPES: ColorTypes = {
+  steel: "bg-[#d5d5d5]",
+  water: "bg-[#59bfff]",
+  bug: "bg-[#c5d895]",
+  dragon: "bg-[#a585d5]",
+  electric: "bg-[#ffffe0]",
+  ghost: "bg-[#993333]",
+  fire: "bg-[#ff6666]",
+  fairy: "bg-[#f5fffa]",
+  ice: "bg-[#b5ffff]",
+  fighting: "bg-[#f0c8c5]",
+  normal: "bg-[#d5d5d5]",
+  grass: "bg-[#95ff95]",
+  psychic: "bg-[#f5ffff]",
+  rock: "bg-[#d5c2b6]",
+  dark: "bg-[#b33333]",
+  ground: "bg-[#e5c5a5]",
+  poison: "bg-[#c5d2a5]",
+  flying: "bg-[#d5f5d5]",
+};
 
 export function PokemonTypes({ types, name }: Props) {
   return (
